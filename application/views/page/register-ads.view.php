7209a291e1835f09a8687e42f46c3f4a14b132b6
NoviHe <claywhoami6@gmail.com>
2023-10-04 18:08:25 +0700

    build Locker

--- application/views/page/register-ads.view.php
+++ application/views/page/register-ads.view.php
@@ -1,152 +1,143 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="robots" content="noindex">
-    <title> Loading...</title>
-    <!--<meta http-equiv="refresh" content="0;url=<?php echo OFFER_LINK1 . $sub_id; ?>">-->
-    <?php
-    require_once ROOT . "/application/functions/functions_html.php";
-    ?>
-    <meta property="og:locale" content="en_US">
-    <meta property="og:title" content="Process...." />
-    <meta property="og:description" content="Process your prize">
-    <meta property="og:type" content="website" />
-    <meta property="og:author" content="NviReview">
-    <meta property="og:site_name" content="<?= SITE_NAME ?>">
-    <meta property="og:url" content="<?= curent_url() ?>" />
-    <meta http-equiv="refresh" content="10;url=https://wokm8isd4zit.com/azfawwqn?key=b4abf509c736f0d2d7218ad5fe4ca84b">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <meta name="robots" content="noindex">
+  <title> Loading...</title>
+  <!--<meta http-equiv="refresh" content="0;url=<?php echo OFFER_LINK1 . $sub_id; ?>">-->
+  <?php
+  require_once ROOT . "/application/functions/functions_html.php";
+  ?>
+  <meta property="og:locale" content="en_US">
+  <meta property="og:title" content="Process...." />
+  <meta property="og:description" content="Process your prize">
+  <meta property="og:type" content="website" />
+  <meta property="og:author" content="NviReview">
+  <meta property="og:site_name" content="<?= SITE_NAME ?>">
+  <meta property="og:url" content="<?= curent_url() ?>" />
+  <meta http-equiv="refresh" content="10;url=https://wokm8isd4zit.com/azfawwqn?key=b4abf509c736f0d2d7218ad5fe4ca84b">
 
-    <style>
-        .loading-redirect {
-            height: 100vh;
-            display: -webkit-box;
-            display: -ms-flexbox;
-            display: flex;
-            -webkit-box-align: center;
-            -ms-flex-align: center;
-            align-items: center;
-            -webkit-box-pack: center;
-            -ms-flex-pack: center;
-            justify-content: center;
-            -webkit-box-orient: vertical;
-            -webkit-box-direction: normal;
-            -ms-flex-direction: column;
-            flex-direction: column;
-        }
+  <style>
+    .loading-redirect {
+      height: 100vh;
+      display: -webkit-box;
+      display: -ms-flexbox;
+      display: flex;
+      -webkit-box-align: center;
+      -ms-flex-align: center;
+      align-items: center;
+      -webkit-box-pack: center;
+      -ms-flex-pack: center;
+      justify-content: center;
+      -webkit-box-orient: vertical;
+      -webkit-box-direction: normal;
+      -ms-flex-direction: column;
+      flex-direction: column;
+    }
 
-        .text-center {
-            text-align: center !important;
-        }
+    .text-center {
+      text-align: center !important;
+    }
 
-        .lds-dual-ring {
-            display: inline-block;
-            width: 64px;
-            height: 64px;
-        }
+    .lds-dual-ring {
+      display: inline-block;
+      width: 64px;
+      height: 64px;
+    }
 
-        .lds-dual-ring:after {
-            content: " ";
-            display: block;
-            width: 46px;
-            height: 46px;
-            margin: 1px;
-            border-radius: 50%;
-            border: 5px solid #222;
-            border-color: #222 transparent #222 transparent;
-            animation: lds-dual-ring 1.2s linear infinite;
-        }
+    .lds-dual-ring:after {
+      content: " ";
+      display: block;
+      width: 46px;
+      height: 46px;
+      margin: 1px;
+      border-radius: 50%;
+      border: 5px solid #222;
+      border-color: #222 transparent #222 transparent;
+      animation: lds-dual-ring 1.2s linear infinite;
+    }
 
-        @keyframes lds-dual-ring {
-            0% {
-                transform: rotate(0deg);
-            }
-
-            100% {
-                transform: rotate(360deg);
-            }
-        }
-        
-       .ads-728x90 {
-        padding-bottom: 10px;
+    @keyframes lds-dual-ring {
+      0% {
+        transform: rotate(0deg);
       }
+
+      100% {
+        transform: rotate(360deg);
+      }
+    }
+
+    .ads-728x90 {
+      padding-bottom: 10px;
+    }
+
+    .ads-468x60 {
+      padding-bottom: 10px;
+    }
+
+    .ads-300x250 {
+      margin-bottom: 10px;
+    }
+
+    .ads-160x300 {
+      margin-top: 10px;
+    }
+
+    @media only screen and (max-width: 750px) {
+      .ads-728x90 {
+        display: none;
+      }
+
       .ads-468x60 {
-        padding-bottom: 10px;
+        display: none;
       }
-      .ads-300x250 {
-        margin-bottom: 10px;
-      }
-      .ads-160x300 {
-        margin-top: 10px;
-      }
-      @media only screen and (max-width: 750px) {
-        .ads-728x90 {
-          display: none;
-        }
-        .ads-468x60 {
-          display: none;
-        }
-      }
-    </style>
-    <?php echo histats_code() ?>
-    <script>document.write(atob("PHNjcmlwdCB0eXBlPSd0ZXh0L2phdmFzY3JpcHQnIHNyYz0nLy9wbDIwNjA2NDA0LmhpZ2hjcG1yZXZlbnVlZ2F0ZS5jb20vYzUvZTQvOTMvYzVlNDkzY2JlMTlmN2QyYzQ2MWI2NmQ1MWE1MWIxODUuanMnPjwvc2NyaXB0Pg=="));</script>
-        <script type='text/javascript' src='//wokm8isd4zit.com/e8/09/36/e809362d1edba019c9a38086aad10308.js'></script>
-        <script type='text/javascript' src='//wokm8isd4zit.com/c5/e4/93/c5e493cbe19f7d2c461b66d51a51b185.js'></script>
+    }
+  </style>
+  <?php echo histats_code() ?>
+  <script>
+    document.write(atob("PHNjcmlwdCB0eXBlPSd0ZXh0L2phdmFzY3JpcHQnIHNyYz0nLy9wbDIwNjA2NDA0LmhpZ2hjcG1yZXZlbnVlZ2F0ZS5jb20vYzUvZTQvOTMvYzVlNDkzY2JlMTlmN2QyYzQ2MWI2NmQ1MWE1MWIxODUuanMnPjwvc2NyaXB0Pg=="));
+  </script>
+  <script>
+    document.write(atob("PHNjcmlwdCB0eXBlPSd0ZXh0L2phdmFzY3JpcHQnIHNyYz0nLy93b2ttOGlzZDR6aXQuY29tL2U4LzA5LzM2L2U4MDkzNjJkMWVkYmEwMTljOWEzODA4NmFhZDEwMzA4LmpzJz48L3NjcmlwdD4="));
+  </script>
+  <script>
+    document.write(atob("PHNjcmlwdCB0eXBlPSd0ZXh0L2phdmFzY3JpcHQnIHNyYz0nLy93b2ttOGlzZDR6aXQuY29tL2M1L2U0LzkzL2M1ZTQ5M2NiZTE5ZjdkMmM0NjFiNjZkNTFhNTFiMTg1LmpzJz48L3NjcmlwdD4="));
+  </script>
+
 </head>
 
 <body class="text-center">
-    <div class="ads-728x90">
-      <!--<img src="ads/728x90.jpg" alt="" />-->
+  <div class="ads-728x90">
+    <!--<img src="ads/728x90.jpg" alt="" />-->
+  </div>
+  <div class="ads-468x60">
+    <!--<img src="ads/468x60.jpg" alt="" />-->
+    <script>
+      document.write(atob("PHNjcmlwdCB0eXBlPSJ0ZXh0L2phdmFzY3JpcHQiPgogICAgICAgIAlhdE9wdGlvbnMgPSB7CiAgICAgICAgCQkna2V5JyA6ICdlYTUyOWQzMTIzYjVkOGU5ZDg4MDlkYmMwMjNkYWRjMycsCiAgICAgICAgCQknZm9ybWF0JyA6ICdpZnJhbWUnLAogICAgICAgIAkJJ2hlaWdodCcgOiA2MCwKICAgICAgICAJCSd3aWR0aCcgOiA0NjgsCiAgICAgICAgCQkncGFyYW1zJyA6IHt9CiAgICAgICAgCX07CiAgICAgICAgCWRvY3VtZW50LndyaXRlKCc8c2NyJyArICdpcHQgdHlwZT0idGV4dC9qYXZhc2NyaXB0IiBzcmM9Ii8vd29rbThpc2Q0eml0LmNvbS9lYTUyOWQzMTIzYjVkOGU5ZDg4MDlkYmMwMjNkYWRjMy9pbnZva2UuanMiPjwvc2NyJyArICdpcHQ+Jyk7CiAgICAgICAgPC9zY3JpcHQ+ICA="));
+    </script>
+  </div>
+  <div class="ads-320x50">
+    <!--<img src="ads/320x50.jpg" alt="" />-->
+    <script>
+      document.write(atob("PHNjcmlwdCB0eXBlPSJ0ZXh0L2phdmFzY3JpcHQiPgogICAgICAgIAlhdE9wdGlvbnMgPSB7CiAgICAgICAgCQkna2V5JyA6ICczNTc5YjhkMjQxZmVhODRkODU3OWEyZjdkNzQxMmE1MScsCiAgICAgICAgCQknZm9ybWF0JyA6ICdpZnJhbWUnLAogICAgICAgIAkJJ2hlaWdodCcgOiA1MCwKICAgICAgICAJCSd3aWR0aCcgOiAzMjAsCiAgICAgICAgCQkncGFyYW1zJyA6IHt9CiAgICAgICAgCX07CiAgICAgICAgCWRvY3VtZW50LndyaXRlKCc8c2NyJyArICdpcHQgdHlwZT0idGV4dC9qYXZhc2NyaXB0IiBzcmM9Ii8vd29rbThpc2Q0eml0LmNvbS8zNTc5YjhkMjQxZmVhODRkODU3OWEyZjdkNzQxMmE1MS9pbnZva2UuanMiPjwvc2NyJyArICdpcHQ+Jyk7CiAgICAgICAgPC9zY3JpcHQ+"));
+    </script>
+  </div>
+  <!-- <img src="ads/160x600.jpg" alt="" /> -->
+  <div class="loading-redirect">
+    <div class="ads-300x250">
+      <!--<img src="ads/300x250.jpg" alt="" />-->
+      <script>
+        document.write(atob("PHNjcmlwdCB0eXBlPSJ0ZXh0L2phdmFzY3JpcHQiPgogICAgICAgIAlhdE9wdGlvbnMgPSB7CiAgICAgICAgCQkna2V5JyA6ICc0YTc1NWQ2NDE2MDhmZGQ5YWRmNjZhYjExNmZkOTcyZScsCiAgICAgICAgCQknZm9ybWF0JyA6ICdpZnJhbWUnLAogICAgICAgIAkJJ2hlaWdodCcgOiAyNTAsCiAgICAgICAgCQknd2lkdGgnIDogMzAwLAogICAgICAgIAkJJ3BhcmFtcycgOiB7fQogICAgICAgIAl9OwogICAgICAgIAlkb2N1bWVudC53cml0ZSgnPHNjcicgKyAnaXB0IHR5cGU9InRleHQvamF2YXNjcmlwdCIgc3JjPSIvL3dva204aXNkNHppdC5jb20vNGE3NTVkNjQxNjA4ZmRkOWFkZjY2YWIxMTZmZDk3MmUvaW52b2tlLmpzIj48L3NjcicgKyAnaXB0PicpOwogICAgICAgIDwvc2NyaXB0Pg=="));
+      </script>
     </div>
-    <div class="ads-468x60">
-         <!--<img src="ads/468x60.jpg" alt="" />-->
-      <script type="text/javascript">
-        	atOptions = {
-        		'key' : 'ea529d3123b5d8e9d8809dbc023dadc3',
-        		'format' : 'iframe',
-        		'height' : 60,
-        		'width' : 468,
-        		'params' : {}
-        	};
-        	document.write('<scr' + 'ipt type="text/javascript" src="//wokm8isd4zit.com/ea529d3123b5d8e9d8809dbc023dadc3/invoke.js"></scr' + 'ipt>');
-        </script>   
+    <div class="lds-dual-ring"></div>
+    <small>Click <a href="https://wokm8isd4zit.com/azfawwqn?key=b4abf509c736f0d2d7218ad5fe4ca84b" target="blank">"Here"</a> if not redirected</small>
+    <div class="ads-160x300">
+      <!--<img src="ads/160x300.jpg" alt="" />-->
     </div>
-    <div class="ads-320x50">
-      <!--<img src="ads/320x50.jpg" alt="" />-->
-      <script type="text/javascript">
-        	atOptions = {
-        		'key' : '3579b8d241fea84d8579a2f7d7412a51',
-        		'format' : 'iframe',
-        		'height' : 50,
-        		'width' : 320,
-        		'params' : {}
-        	};
-        	document.write('<scr' + 'ipt type="text/javascript" src="//wokm8isd4zit.com/3579b8d241fea84d8579a2f7d7412a51/invoke.js"></scr' + 'ipt>');
-        </script>
-    </div>
-    <!-- <img src="ads/160x600.jpg" alt="" /> -->
-    <div class="loading-redirect">
-      <div class="ads-300x250">
-        <!--<img src="ads/300x250.jpg" alt="" />-->
-        <script type="text/javascript">
-        	atOptions = {
-        		'key' : '4a755d641608fdd9adf66ab116fd972e',
-        		'format' : 'iframe',
-        		'height' : 250,
-        		'width' : 300,
-        		'params' : {}
-        	};
-        	document.write('<scr' + 'ipt type="text/javascript" src="//wokm8isd4zit.com/4a755d641608fdd9adf66ab116fd972e/invoke.js"></scr' + 'ipt>');
-        </script>
-      </div>
-      <div class="lds-dual-ring"></div>
-      <small>Click <a href="https://wokm8isd4zit.com/azfawwqn?key=b4abf509c736f0d2d7218ad5fe4ca84b" target="blank">"Here"</a> if not redirected</small>
-      <div class="ads-160x300">
-        <!--<img src="ads/160x300.jpg" alt="" />-->
-      </div>
-    </div>
+  </div>
 </body>
 
 </html>
