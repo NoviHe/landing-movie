4d75b97ce3b98ca1afb99f4787bac8c464dbf5f6
NoviHe <claywhoami6@gmail.com>
2023-10-04 19:56:37 +0700

    add git ignore

--- config/config.php
+++ config/config.php
@@ -2,6 +2,8 @@
 define('DEVELOPMENT_ENVIRONMENT', true);
 define('BASE_PATH', 'http://localhost/landing-novi2');
 define('URL_WEBSITE', 'http://landing-novi2.test');
+// define('BASE_PATH', 'https://nvireview.com');
+// define('URL_WEBSITE', 'https://nvireview.com');
 define('DEFAULT_CONTROLLER', 'home');
 define('SITES_URL', $_SERVER['HTTP_HOST']);
 define('DOCUMENT_ROOT', $_SERVER['DOCUMENT_ROOT']);
