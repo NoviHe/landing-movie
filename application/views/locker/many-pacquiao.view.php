4ae704c0498c243d0d49b9e8a12ef1def5b13093
NoviHe <claywhoami6@gmail.com>
2023-10-04 19:37:45 +0700

    add google translate

--- application/views/locker/many-pacquiao.view.php
+++ application/views/locker/many-pacquiao.view.php
@@ -9,7 +9,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Manny Pacquiao</title>
     <style>
         .tombolKustom {
             box-shadow: 3px 4px 0px 0px #00bfff;
@@ -44,6 +44,12 @@
     <section class="container">
         <div class="row mt-5 pt-2 text-center">
             <div class="col-md-12 mb-2">
+                <div class="available-formats"><b>
+                        <font color="black">SELECT YOUR LANGUAGE :</font>
+                    </b></div>
+                <div id="google_translate_element"></div>
+            </div>
+            <div class="col-md-12 mb-2">
                 <h4>Manny Pacquiao</h4>
                 <img class="img-thumbnail" src="<?= BASE_PATH; ?>/public/img/many-pacquiao/photo_2023-09-18_19-08-24.jpg" alt="">
             </div>
@@ -70,6 +76,15 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
     -->
+    <script type="text/javascript">
+        function googleTranslateElementInit() {
+            new google.translate.TranslateElement({
+                pageLanguage: 'en',
+                layout: google.translate.TranslateElement.InlineLayout.VERTIKAL
+            }, 'google_translate_element');
+        }
+    </script>
+    <script type="text/javascript" src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
     <script type="text/javascript">
         var oDajn_mzp_uWDnuc = {
             "it": 4178726,
