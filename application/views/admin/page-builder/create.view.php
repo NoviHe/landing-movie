b06a7d416bf79ddd0a74b394dca9db8408cc12ac
NoviHe <claywhoami6@gmail.com>
2023-10-07 17:55:53 +0700

    create builder

--- application/views/admin/page-builder/create.view.php
+++ application/views/admin/page-builder/create.view.php
@@ -62,34 +62,34 @@
             </div>
             <div class="mb-3">
                 <div class="row">
-                    <div class="col-3">
+                    <div class="col-md-3">
                         <label for="btn_name" class="form-label">Button Name</label>
                         <input type="text" name="btn_name" class="form-control" id="btn_name" placeholder="Button Name" value="ENTER HERE!">
                     </div>
-                    <div class="col-3">
+                    <div class="col-md-3">
                         <label for="btn_class" class="form-label">Button Class</label>
                         <input type="text" name="btn_class" class="form-control" id="btn_class" placeholder="Button Class" value="tombolKustom">
                     </div>
-                    <div class="col-6">
+                    <div class="col-md-6">
                         <label for="btn_type" class="form-label">Button Type</label>
-                        <select name="btn_type" id="btn_type" class="form-control">
+                        <select name="btn_type" id="btn_type" class="form-control" required>
                             <option value="">PILIH TYPE</option>
                             <option value="a">&lt;a href="" &gt;&lt;/a&gt;</option>
                             <option value="btn" selected>&lt;button type="" &gt;&lt;/button&gt;</option>
                         </select>
                     </div>
-                    <div class="col-3">
+                    <div class="col-md-3 btn_type_selected">
                         <label for="btn_onclick" class="form-label">Button Onclick</label>
                         <input type="text" name="btn_onclick" class="form-control" id="btn_onclick" placeholder="Button Onclick" value="_tx()">
                     </div>
-                    <div class="col-9">
+                    <div class="col-md-9 btn_type_selected">
                         <p class="pt-4"><small>isi jika pilih button type nya &lt;button type="" &gt;&lt;/button&gt;</small></p>
                     </div>
-                    <div class="col-3">
+                    <div class="col-md-3 a_href_type_selected">
                         <label for="btn_target" class="form-label">Button Target </label>
                         <input type="text" name="btn_target" class="form-control" id="btn_target" placeholder="Button Target" value="_blank">
                     </div>
-                    <div class="col-9">
+                    <div class="col-md-9 a_href_type_selected">
                         <label for="btn_link" class="form-label">Button Link</label>
                         <input type="text" name="btn_link" class="form-control" id="btn_link" placeholder="Button Link" value="">
                         <small>isi jika pilih button type nya &lt;a href="" &gt;&lt;/a&gt;</small>
@@ -97,20 +97,37 @@
                 </div>
             </div>
             <div class="mb-3">
-                <label for="gambarFoter" class="form-label">Gambar Footer</label>
-                <input class="form-control" type="file" name="gambar2" id="gambarFoter">
-            </div>
-            <div class="mb-3">
-                <label for="gambar_url2" class="form-label">Gambar Url Footer</label>
-                <input type="text" name="gambar_url2" class="form-control" id="gambar_url2" placeholder="https://imgur.com/xxxxxxxx.jpg">
-                <small>Isi Salah satu Sumber gambar</small>
+                <div class="row">
+                    <div class="col-md-4">
+                        <label for="pilih_tipe_upload_gambar" class="form-label">Pilih Sumber Gambar</label><br>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="sumber_gambar" id="sumber_upload" value="upload">
+                            <label class="form-check-label" for="sumber_upload">Upload</label>
+                        </div>
+                        <div class="form-check form-check-inline">
+                            <input class="form-check-input" type="radio" name="sumber_gambar" id="sumber_url" value="url">
+                            <label class="form-check-label" for="sumber_url">Url</label>
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="mb-3 sumber_type_upload">
+                            <label for="gambarFoter" class="form-label">Gambar Footer</label>
+                            <input class="form-control" type="file" name="gambar2" id="gambarFoter">
+                        </div>
+                        <div class="mb-3 sumber_type_url">
+                            <label for="gambar_url2" class="form-label">Gambar Url Footer</label>
+                            <input type="text" name="gambar_url2" class="form-control" id="gambar_url2" placeholder="https://imgur.com/xxxxxxxx.jpg">
+                        </div>
+                    </div>
+                </div>
+
             </div>
             <div class="mb-3">
                 <label for="text_footer" class="form-label">Text Footer</label>
                 <input type="text" name="text_footer" class="form-control" id="text_footer" placeholder="Text Footer" value="Copyright © 2023 | Footer.">
             </div>
             <div class="mb-3">
-                <label for="custom_css" class="form-label">Kustom CSS</label>
+                <label for="custom_css" class="form-label">Kustom CSS</label> <button type="button" class="btn btn-sm btn-primary float-end" id="btn_hide_kustom_css">Hide</button>
                 <textarea name="custom_css" class="richText" id="custom_css">
             .tombolKustom {
                 box-shadow: 3px 4px 0px 0px #00bfff;
@@ -142,7 +159,7 @@
             </textarea>
             </div>
             <div class="mb-3">
-                <label for="script_locker" class="form-label">Script Locker</label>
+                <label for="script_locker" class="form-label">Script Locker</label> <button type="button" class="btn btn-sm btn-primary float-end" id="btn_hide_script_locker">Hide</button>
                 <textarea name="script_locker" class="richText" id="script_locker">
             <script type="text/javascript">
         var oDajn_mzp_uWDnuc = {
@@ -179,6 +196,44 @@
     </div>
 </form>
 <script>
+    $(document).ready(function() {
+        var selected = $('#btn_type').val();
+        select_selected(selected);
+
+    })
+    $('#btn_type').on('change', function(e) {
+        var selected = $(this).val();
+        select_selected(selected);
+    })
+
+    $('input[type=radio][name=sumber_gambar]').on('change', function() {
+        switch ($(this).val()) {
+            case 'upload':
+                $('.sumber_type_url').hide(2000);
+                $('.sumber_type_upload').show(2000);
+                break;
+            case 'url':
+                $('.sumber_type_url').show(2000);
+                $('.sumber_type_upload').hide(2000);
+                break;
+        }
+    });
+
+    function select_selected(selected) {
+        if (selected != '') {
+            if (selected == 'a') {
+                $('.btn_type_selected').hide(2000);
+                $('.a_href_type_selected').show(2000);
+            } else if (selected == 'btn') {
+                $('.btn_type_selected').show(2000);
+                $('.a_href_type_selected').hide(2000);
+            }
+        } else {
+            $('.btn_type_selected').hide(2000);
+            $('.a_href_type_selected').hide(2000);
+        }
+    }
+
     $('#text_body').richText({
         fontList: ["Arial",
             "Arial Black",
